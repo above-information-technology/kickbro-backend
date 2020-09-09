15d27b33d18a4e8efea135fa09c542f60739e200
Stefan Danescu <stefi_d27@yahoo.com>
2020-09-09 17:40:29 +0300

    change database cluster

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-db = mongoose.connect('mongodb+srv://kickbro_user:th3k1ckbr0app@kickbro.v45s5.mongodb.net/kickbro_emails?retryWrites=true&w=majority', {
+db = mongoose.connect('mongodb+srv://kickbro_user:th3k1ckbr0app@cluster0.v45s5.mongodb.net/kickbro_emails?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
