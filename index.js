0b626a93051de38bc59a1447b0124d4d43c2ffb5
Stefan Danescu <stefi_d27@yahoo.com>
2020-09-09 12:01:34 +0300

    added email filter

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000'
-  }));
+}));
 
 const port = process.env.PORT || 3000
 
 app.post('/email', async (req, res) => {
+    const isEmail = await Email.find({ email: req.body.email});
+    if (isEmail.length) {
+        return res.status(404).send('Email already exists')
+    }
     const email = new Email(req.body)
-
     try {
         await email.save()
-        res.status(201).send('Success')
+        return res.status(201).send('Success')
     } catch (e) {
-        res.status(400).send(e.message)
+        return res.status(400).send(e.message)
     }
 })
 
