c961066c154a8cdea58c4984f988cf16a4a970da
Stefan Danescu <stefi_d27@yahoo.com>
2020-09-10 03:34:50 +0300

    afeavav

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/email', async (req, res) => {
     const email = new Email(req.body)
     try {
         await email.save()
-        return res.status(201).send('Success')
+        return res.status(201).send('Success aaaa')
     } catch (e) {
         return res.status(404).send(e.message)
     } 
